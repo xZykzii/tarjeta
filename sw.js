7a603f9dd968f6078b234800aa55194a7a5bee6c
xZykzii <miguelitoescare90@gmail.com>
2025-11-29 15:45:58 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "cuotas-v1";
+const OFFLINE_URLS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+  // agrega más archivos si quieres cachearlos offline
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
